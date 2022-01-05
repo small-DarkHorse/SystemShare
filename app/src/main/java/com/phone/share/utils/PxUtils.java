2cb40d3f17c97e7fd6f432c6e18214cc603f35ef
wzk <xiaokai.dana@foxmail.com>
2022-01-05 10:34:07 +0800

    PX转换

---
+++ app/src/main/java/com/phone/share/utils/PxUtils.java
@@ -0,0 +1,31 @@
+package com.phone.share.utils;
+
+import android.content.Context;
+
+/**
+ * @author WangZhenKai
+ * @ClassName : PxUtils
+ * @Date 2021/1/4
+ * @Description : 单位转换器
+ */
+public class PxUtils {
+    public static int px2dp(Context context, float pxValue) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    public static int dp2px(Context context, float dpValue) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    public static int px2sp(Context context, float pxValue) {
+        float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    public static int sp2px(Context context, float spValue) {
+        float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+}
